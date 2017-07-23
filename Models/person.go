7bd9668f6bbb2c6acb38208b4d5b5c992e509f6f
Alaa Attya <alaa.attya@tajawal.com>
2017-07-23 18:55:30 +0200

    Added: split package into files

---
+++ Models/person.go
@@ -0,0 +1,11 @@
+package recipy
+
+import "github.com/go-bongo/bongo"
+
+// Person model
+type Person struct {
+	bongo.DocumentBase `bson:",inline"`
+	FirstName          string
+	LastName           string
+	Gender             string
+}
--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-bongo/bongo"
 )
 
 func main() {
+	config := &bongo.Config{
+		ConnectionString: "192.168.99.100:37017",
+		Database:         "bongotest",
+	}
+
+	connection, err := bongo.Connect(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	myPerson := &Person{
+		FirstName: "Testy",
+		LastName:  "McGee",
+		Gender:    "male",
+	}
+	errs := connection.Collection("people").Save(myPerson)
+
+	if vErr, ok := errs.(*bongo.ValidationError); ok {
+		fmt.Println("Validation errors are:", vErr.Errors)
+	} else {
+		fmt.Println("Got a real error:")
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
